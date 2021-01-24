6b5e421dd957043eb70f593b6c7e73b187c83af3
Clannis <Dodds1990@gmail.com>
2021-01-23 20:51:26 -0600

    change buttons to type submit and add logic to differentiate action

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,34 +14,31 @@ class Login extends Component {
         this.setState({[event.target.name]: event.target.value})
     }
 
-    handleLogin = (event) => {
+    handleSubmit = (event) => {
         event.preventDefault()
         const email = this.state.email
         const password = this.state.password
         const user = {password: password, email: email}
-        this.props.login(user)
-    }
-
-    handleRegister = (event) => {
-        event.preventDefault()
-        const email = this.state.email
-        const password = this.state.password
-        const user = {password: password, email: email}
-        this.props.register(user)
+        if (event.submit === "Login") {
+            this.props.login(user)
+        } else {
+            this.props.register(user)
+        }
+        
     }
 
     render() {
         return(
             <section className="welcome">
                 <h1 className="welcome__heading">Welcome to Dog Land Academy</h1>
-                <form className="welcome__form">
-                    <input className="welcome__form--input" type="email" id="inputEmail" name="email"  placeholder="Email Address" required="" autoFocus="" onChange={this.handleChange}/>
-                    <input className="welcome__form--input" type="password" id="inputPassword" name="password" placeholder="Password" required="" onChange={this.handleChange}/>
+                <form className="welcome__form" onSubmit={this.handleSubmit}>
+                    <input className="welcome__form--input" type="email" id="inputEmail" name="email"  placeholder="Email Address" required autoFocus="" onChange={this.handleChange}/>
+                    <input className="welcome__form--input" type="password" id="inputPassword" name="password" placeholder="Password" required onChange={this.handleChange}/>
+                    <div className="welcome__form--submit-group">
+                        <button className="welcome__form--submit btn" type="submit" name="submit">Login</button>
+                        <button className="welcome__form--submit btn" type="submit" name="submit">Register</button>
+                    </div>
                 </form>
-                <div className="welcome__form--submit-group">
-                    <button className="welcome__form--submit btn" onClick={this.handleLogin}>Login</button>
-                    <button className="welcome__form--submit btn" onClick={this.handleRegister}>Register</button>
-                </div>
                 <p className="welcome__seperator welcome__seperator--or">or</p>
                 <button className="github btn">
                     <img className="github__icon" src={process.env.PUBLIC_URL + '/github-logo.png'} alt="github-logo"></img>
