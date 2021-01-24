645a21386f9ae2d262babcdfb13f3479fb9bb159
Clannis <dodds1990@gmail.com>
2021-01-24 17:07:51 -0600

    add footer content and styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,25 @@ class Footer extends Component {
     
     render() {
         return(
-            <footer>
-                
+            <footer class="footer">
+                <div class="row">
+                    <div class="col-1-of-2">
+                        <div class="footer__navigation">
+                            <ul class="footer__list">
+                                <li class="footer__item"><a href="#" class="footer__link">Login</a></li>
+                                <li class="footer__item"><a href="#" class="footer__link">Testimonials</a></li>
+                                <li class="footer__item"><a href="#" class="footer__link">Contact us</a></li>
+                                <li class="footer__item"><a href="#" class="footer__link">Privacy policy</a></li>
+                                <li class="footer__item"><a href="#" class="footer__link">Terms</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-1-of-2">
+                        <p class="footer__copyright">
+                            Built by <a href="https://jamesrdodds.com/" class="footer__link">James Dodds</a> for <a href="https://flatironschool.com" class="footer__link">Flatiron School's</a> Software Engineering Online course. Copyright &copy; by James Dodds. You are 100% percent allowed to use this webpage for both personal and commercial use, but NOT to claim it as your own design. A credit to the original author, James Dodds, is of course highly appreciated!
+                        </p>
+                    </div>
+                </div>
             </footer>
         )
     }
