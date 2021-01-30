8560f25d4a44a40a2fd03f7e66514c31758d5d2e
Clannis <dodds1990@gmail.com>
2021-01-30 14:30:55 -0600

    create Navlink component and config file to hold list of data

---
+++ src/components/Navlink.js
@@ -0,0 +1,8 @@
+
+const Navlink = (props) => {
+    return(
+        <li class="navigation__item">{props.link.text}</li>
+    )
+}
+
+export default Navlink
---
+++ src/config.js
@@ -0,0 +1,14 @@
+export const navlinks = [
+    {
+        text: "About Dogland",
+        href: "/about"
+    },
+    {
+        text: "Login",
+        href: "/login"
+    },
+    {
+        text: "Signup",
+        href: "/signup"
+    }
+]
