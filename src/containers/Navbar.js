5616ed9e674a90c2f0f9c1c648c5e12780c94dc5
Clannis <dodds1990@gmail.com>
2021-01-30 14:29:41 -0600

    sadd Navbar with list of navlinks

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,12 +1,21 @@
 import { Component } from "react";
+import Navlink from "../components/Navlink";
+import { navlinks } from "../config";
 import "../styles/navbar.scss"
 
 class Navbar extends Component {
+
+    renderNavlinks = () => {
+        return navlinks.map( link => <Navlink link={link}/>)
+    }
     
     render() {
         return(
-            <>
-            </>
+            <nav class="navigation__nav">
+                <ul class="navigation__list">
+                    {this.renderNavlinks()}
+                </ul>
+            </nav>
         )
     }
 }
