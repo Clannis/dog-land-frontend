18ca7ff5ce024fddb89a0719eb2f3c81e6f025fb
Clannis <dodds1990@gmail.com>
2021-02-01 15:07:32 -0600

    Change Routes to Switch architecture

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './styles/app.scss';
 import Login from './pages/Login';
-import {Route} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import Welcome from './pages/Welcome';
 
 function App() {
   return (
-    <>
+    <Switch>
       <Route exact path="/" component={Welcome}/>
       <Route path="/login" component={Login}/>
-    </>
+    </Switch>
   );
 }
 
