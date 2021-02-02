054522b9d33805aaac67df37c7acd19c1651a222
Clannis <dodds1990@gmail.com>
2021-02-02 12:51:22 -0600

    Make rNavlink components link to specified page using react-router-dom link component

--- src/components/Navlink.js
+++ src/components/Navlink.js
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom"
 
 const Navlink = (props) => {
     return(
-        <li className="navigation__item">
-            <span className="navigation__item--text">{props.link.text}</span>
-        </li>
+        <Link to={props.link.href}>
+            <li className="navigation__item">
+                <span className="navigation__item--text">{props.link.text}</span>
+            </li>
+        </Link>
     )
 }
 
