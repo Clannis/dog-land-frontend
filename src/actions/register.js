ddefbb2d9ddf4903d8959f59f8ad35990a3abb6d
Clannis <dodds1990@gmail.com>
2021-02-03 13:09:51 -0600

    Create register action

---
+++ src/actions/register.js
@@ -0,0 +1,21 @@
+import { BASE_URL } from '../config'
+
+export default function login(user) {
+    return (dispatch) => {
+        fetch(`${BASE_URL}/users`, {
+            method: 'POST', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(response => response.json())
+        .then(response => {
+            if (!response.errors) {
+                dispatch({ type: 'LOGIN', user: response.user, token: response.token })
+            } else {
+                alert(`${response.errors}`)
+            }
+        })
+    }
+  }
