a35021e9555d1c9ea603673430b8ffaa8003ac1a
Clannis <dodds1990@gmail.com>
2021-02-10 02:49:46 -0600

    Add user's courses information block to usershow page

--- src/pages/UserShow.js
+++ src/pages/UserShow.js
@@ -5,17 +5,33 @@ class UserShow extends Component {
     render() {
         return (
             <main className="background">
-                <div className="user__user-box">
-                    <div className="user__user-box--img">
+                <section className="user">
+                    <div className="user__user-box">
+                        <div className="user__user-box--img">
 
+                        </div>
+                        <div className="user__user-box--text">
+                            <h2 className="user__user-box--username">{this.props.user.username}</h2>
+                            <p className="user__user-box--name">{this.props.user.name}</p>
+                            <p className="user__user-box--phone-number">{this.props.user.phoneNumber}</p>
+                            <p className="user__user-box--email">{this.props.user.email}</p>
+                        </div>
                     </div>
-                    <div className="user__user-box--text">
-                        <h2 className="user__user-box--username">{this.props.user.username}</h2>
-                        <p className="user__user-box--name">{this.props.user.name}</p>
-                        <p className="user__user-box--phone-number">{this.props.user.phoneNumber}</p>
-                        <p className="user__user-box--email">{this.props.user.email}</p>
+                    <div className="user__course-box">
+                        <div className="user__course-box--courses">
+                            <p>Courses Enrolled:</p>
+                        </div>
+                        <div className="user__course-box--completed">
+                            <p>Courses Completed:</p>
+                        </div>
+                        <div className="user__course-box--enrolled">
+                            <p>Dogs Enrolled:</p>
+                        </div>
                     </div>
-                </div>
+                </section>
+                <section className="user__dogs">
+
+                </section>
             </main>
         )
     }
