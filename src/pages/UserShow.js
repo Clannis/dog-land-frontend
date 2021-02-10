5af9601d6b72c7c1b3548b9c1f49f283104c0576
Clannis <dodds1990@gmail.com>
2021-02-10 02:45:29 -0600

    Add user info block to usershow page pulling from connected state user

--- src/pages/UserShow.js
+++ src/pages/UserShow.js
@@ -1,12 +1,30 @@
 import { Component } from "react";
+import { connect } from "react-redux";
 
 class UserShow extends Component {
     render() {
         return (
-            <>
-            </>
+            <main className="background">
+                <div className="user__user-box">
+                    <div className="user__user-box--img">
+
+                    </div>
+                    <div className="user__user-box--text">
+                        <h2 className="user__user-box--username">{this.props.user.username}</h2>
+                        <p className="user__user-box--name">{this.props.user.name}</p>
+                        <p className="user__user-box--phone-number">{this.props.user.phoneNumber}</p>
+                        <p className="user__user-box--email">{this.props.user.email}</p>
+                    </div>
+                </div>
+            </main>
         )
     }
 }
 
-export default UserShow
+const mapStateToProps = (state) => {
+    return {
+        user: state.user.user
+    }
+}
+
+export default connect(mapStateToProps)(UserShow)
