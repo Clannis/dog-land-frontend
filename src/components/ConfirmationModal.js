0eedd0b25b45ebebf901a85bbdbf7aecbe489a25
Clannis <dodds1990@gmail.com>
2021-02-17 22:51:46 -0600

    Add Styling for ConfirmationModal buttons

--- src/components/ConfirmationModal.js
+++ src/components/ConfirmationModal.js
@@ -2,13 +2,15 @@
 const ConfirmationModal = (props) => {
         return (
             <>
-                <h2>Are You Sure?</h2>
-                <button className="btn" onClick={() => {
-                    props.setConfirmation(true)
-                    }}>Yes</button>
-                <button className="btn" onClick={() => {
-                    props.setConfirmation(false)
-                    }}>Cancel</button>
+                <h2 className="dog-form__heading">Are You Sure?</h2>
+                <div className="spacing__container">
+                    <button className="dog-form__btn btn spacing__padding" onClick={() => {
+                        props.setConfirmation(true)
+                        }}>Yes</button>
+                    <button className="dog-form__btn btn spacing__padding" onClick={() => {
+                        props.setConfirmation(false)
+                        }}>Cancel</button>
+                </div>
             </>
         )
 }
