7f5b0e79ce77e8d7f695497aec199be560ed439b
Clannis <dodds1990@gmail.com>
2021-02-17 23:03:18 -0600

    Correct naming structure

--- src/components/ConfirmationModal.js
+++ src/components/ConfirmationModal.js
@@ -4,10 +4,10 @@ const ConfirmationModal = (props) => {
             <>
                 <h2 className="dog-form__heading">Are You Sure?</h2>
                 <div className="spacing__container">
-                    <button className="dog-form__btn btn spacing__padding" onClick={() => {
+                    <button className="dog-form__btn btn spacing__margin-auto" onClick={() => {
                         props.setConfirmation(true)
                         }}>Yes</button>
-                    <button className="dog-form__btn btn spacing__padding" onClick={() => {
+                    <button className="dog-form__btn btn spacing__margin-auto" onClick={() => {
                         props.setConfirmation(false)
                         }}>Cancel</button>
                 </div>
