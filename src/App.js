d2324d898dc699191968b58ed1c26c6375ccc4c2
clannis <dodds1990@gmail.com>
2021-03-13 12:00:26 -0600

    Update imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './styles/main.scss';
 import Login from './pages/Login';
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import Welcome from './pages/Welcome';
 import TrainerSignup from './pages/TrainerSignup';
 import Navbar from './containers/Navbar';
@@ -10,7 +10,6 @@ import { connect } from 'react-redux';
 import UserShow from './pages/UserShow';
 import TrainerShow from './pages/TrainerShow';
 import autoLogin from './actions/autoLogin';
-import {withRouter} from 'react-router-dom';
 import UserProfile from './pages/UserProfile';
 
 class App extends Component {
