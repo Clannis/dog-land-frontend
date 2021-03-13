0fe93afee5984580add1b0a75f2d086aee2db810
clannis <dodds1990@gmail.com>
2021-03-13 11:31:07 -0600

    Create UserProfile component

---
+++ src/pages/UserProfile.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class UserProfile extends Component {
+
+    render() {
+        return(
+            <main>Hi</main>
+        )
+    }
+
+}
+
+export default UserProfile
