9af7b23b7a512fb2289208a46a4fe99214b5ea71
clannis <dodds1990@gmail.com>
2021-03-19 19:06:20 -0500

    Add subheading to confirmation modal

--- src/components/ConfirmationModal.js
+++ src/components/ConfirmationModal.js
@@ -3,6 +3,7 @@ const ConfirmationModal = (props) => {
         return (
             <>
                 <h2 className="dog-form__heading">Are You Sure?</h2>
+                <h3 className="dog-form__sub-heading">All data relating to this dog will be deleted forever.</h3>
                 <div className="spacing__container">
                     <button className="dog-form__btn btn spacing__margin-auto" onClick={() => {
                         props.setConfirmation(true)
